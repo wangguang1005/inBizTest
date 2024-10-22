8664ad45946e604dd4fa0779b09dc161303ff1a8
王光明森 <8520353+wang-guangmingsen@user.noreply.gitee.com>
2024-10-22 13:19:25 +0800

    初始化

---
+++ custom/wap/pages/gengduozhishi/extend.js
@@ -0,0 +1,2 @@
+let id = inbiz.utils.getUrlParameter('id') 
+console.log('传过来的ID',id)
---
+++ custom/wap/pages/index/extend.js
@@ -0,0 +1,4 @@
+// inbiz('SearchEngine_m10wquy936').decorator.className="floatLeft"
+// inbiz('Message_02k3b4z5b3q').decorator.className="floatLeft"
+// inbiz('Message_sdwmlrozh1g').decorator.className="floatLeft"
+// inbiz('EformHidden_zr7ulucwvxi').decorator.className="clear"
